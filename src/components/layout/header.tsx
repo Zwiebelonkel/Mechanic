d6280c53de4ced0199d975a91ba4b60c2d08bd9c
Luca Müller <lucamuller2004@gmail.com>
2025-10-22 14:51:43 +0000

    the title antons werkstatt soll immer sichbar sein, nicht nur bei ausgek

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -8,13 +8,13 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="flex items-center space-x-2">
-            <SidebarTrigger />
-             <Link href="/" className="flex items-center space-x-2 md:hidden">
-                <Car className="h-6 w-6" />
-                <span className="font-bold sm:inline-block">
-                    Anton's Werkstatt
-                </span>
-            </Link>
+          <SidebarTrigger />
+          <Link href="/" className="flex items-center space-x-2">
+            <Car className="h-6 w-6" />
+            <span className="font-bold sm:inline-block">
+              Anton's Werkstatt
+            </span>
+          </Link>
         </div>
         <nav className="flex items-center gap-1 text-sm ml-auto">
           <Button asChild>
