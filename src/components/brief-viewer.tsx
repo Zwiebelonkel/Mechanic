5d4c431b36087b1c3527a28a3d1447833d7e0867
Luca Müller <lucamuller2004@gmail.com>
2025-10-24 14:53:15 +0000

    okay, please enable controls

--- src/components/brief-viewer.tsx
+++ src/components/brief-viewer.tsx
@@ -10,6 +10,7 @@ const BriefViewer = () => {
       src="/models/brief.glb"
       alt="A 3D model of a letter"
       auto-rotate
+      camera-controls
       shadow-intensity="1"
       style={{ width: '100%', height: '100%', minHeight: '400px' }}
     ></model-viewer>
