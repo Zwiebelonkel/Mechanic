e38b7b0604b0593f3cd8d03033e795d944d43726
Luca Müller <lucamuller2004@gmail.com>
2025-10-24 15:09:49 +0000

    brief

--- src/components/brief-viewer.tsx
+++ src/components/brief-viewer.tsx
@@ -6,16 +6,17 @@ import React from 'react';
 
 const BriefViewer = () => {
   return (
-    <model-viewer
-      src="/models/brief.glb"
-      alt="A 3D model of a letter"
-      auto-rotate
-      camera-controls
-      shadow-intensity="1"
-      environment-image="/images/garage.jpg"
-      exposure="1.5"
-      style={{ width: '100%', height: '100%', minHeight: '400px' }}
-    ></model-viewer>
+<model-viewer
+  src="/models/brief.glb"
+  alt="A 3D model of a letter"
+  auto-rotate
+  camera-controls
+  shadow-intensity="1"
+  environment-image="/images/garage.jpg"
+  tone-mapping-exposure="2.5"
+  style={{ width: '100%', height: '100%', minHeight: '400px' }}
+></model-viewer>
+
   );
 };
 
