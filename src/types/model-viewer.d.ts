9d40ea9c7481bcae2be1d2a6dd395ca90ad9fbdd
Luca Müller <lucamuller2004@gmail.com>
2025-10-28 09:28:22 +0000

    ./src/components/bmw-viewer.tsx:11:7 Type error: Type '{ src: string; po

--- src/types/model-viewer.d.ts
+++ src/types/model-viewer.d.ts
@@ -4,11 +4,16 @@ declare namespace JSX {
       React.HTMLAttributes<HTMLElement> & {
         src: string;
         alt: string;
+        poster?: string;
         'camera-controls'?: boolean;
         'auto-rotate'?: boolean;
         ar?: boolean;
         'ar-modes'?: string;
         'shadow-intensity'?: string;
+        'disable-zoom'?: boolean;
+        'disable-pan'?: boolean;
+        'field-of-view'?: string;
+        'environment-image'?: string;
         style?: React.CSSProperties;
       },
       HTMLElement
